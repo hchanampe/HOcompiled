2dde9034b140f5d41c156d24f63d42e4a1fa676f
hchanampe <hugogher@gmail.com>
2019-07-17 16:42:38 -0300

    llamado de funcion de fortran desde C

--- multilenguaje/05-f-from-c/c-main.c
+++ multilenguaje/05-f-from-c/c-main.c
@@ -1,18 +1,22 @@
 
 #include <stdio.h>
-#include "c-sum.h"
 
 #define NUM 200
+
+extern void sum_abs_(int *a,int *b,int *s);
 
 int main(int argc, char **argv)
 {
     int data[NUM], num, i;
+    int s;
 
     num = NUM;
     for (i=0; i < num; i++) {
         data[i] = i - 100 + 1;  /* NOTE: difference in array numbering */  
     }
 
-    printf("sum=%d\n", sum_abs(data, num));
+    //printf("sum=%d\n", sum_abs_(data, &num));
+	sum_abs_(data, &num,&s);
+	printf("sum=%d\n",s);
     return 0;
 }
